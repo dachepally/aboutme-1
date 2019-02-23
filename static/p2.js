727a386f2e053dd8b95686592eacaa770fe5dbcc
Pavan <s534625@nwmissouri.edu>
2019-02-22 18:58:04 -0600

    Changes

--- static/p2.js
+++ static/p2.js
@@ -1,3 +1,5 @@
+var regex=/^[0-9]+$/;
+
 let getsum = function (arg1, arg2) {
   return parseInt(arg1) + parseInt(arg2);
 }
@@ -26,38 +28,59 @@ function mouseOut() {
 
 
 function sum() {
-  num1 = document.getElementById("value1").value;
-  num2 = document.getElementById("value2").value;
-  let sumVar=getsum(num1, num2);
+  localStorage.setItem("num1",document.getElementById("value1").value);
+  localStorage.setItem("num2",document.getElementById("value2").value);
+  let sumVar=getsum(localStorage.getItem("num1"), localStorage.getItem("num2"));
+  if(localStorage.getItem("num1").match(regex) && localStorage.getItem("num2").match(regex)){
   localStorage.setItem("sum1", sumVar);
   $("#result").html("Sum of two numbers is : " + localStorage.getItem("sum1"));
+  }
+  else{
+    throw Error('The given argument is not a number');
+  }
 }
+
 let getdiff = function (arg1, arg2) {
   return parseInt(arg1) - parseInt(arg2);
 }
 function diff() {
-  num1 = document.getElementById("value1").value;
-  num2 = document.getElementById("value2").value;
-  localStorage.setItem("diff1", getdiff(num1, num2));
+  localStorage.setItem("num1",document.getElementById("value1").value);
+  localStorage.setItem("num2",document.getElementById("value2").value);
+  if(localStorage.getItem("num1").match(regex) && localStorage.getItem("num2").match(regex)){
+  localStorage.setItem("diff1", getdiff(localStorage.getItem("num1"), localStorage.getItem("num2")));
   $("#result").html("Difference of two numbers is : " + localStorage.getItem("diff1"));
+  }
+  else{
+    throw Error('The given argument is not a number');
+  }
 }
 let getmul = function (arg1, arg2) {
   return parseInt(arg1) * parseInt(arg2);
 }
 function mul() {
-  num1 = document.getElementById("value1").value;
-  num2 = document.getElementById("value2").value;
-  localStorage.setItem("mul1", getmul(num1, num2));
+  localStorage.setItem("num1",document.getElementById("value1").value);
+  localStorage.setItem("num2",document.getElementById("value2").value);
+  if(localStorage.getItem("num1").match(regex) && localStorage.getItem("num2").match(regex)){
+  localStorage.setItem("mul1", getmul(localStorage.getItem("num1"), localStorage.getItem("num2")));
   $("#result").html("Product of two numbers is : " + localStorage.getItem("mul1"));
+  }
+  else{
+    throw Error('The given argument is not a number');
+  }
 }
 let getdiv = function (arg1, arg2) {
   return parseInt(arg1) / parseInt(arg2);
 }
 function div() {
-  num1 = document.getElementById("value1").value;
-  num2 = document.getElementById("value2").value;
-  localStorage.setItem("div1", getdiv(num1, num2));
+  localStorage.setItem("num1",document.getElementById("value1").value);
+  localStorage.setItem("num2",document.getElementById("value2").value);
+  if(localStorage.getItem("num1").match(regex) && localStorage.getItem("num2").match(regex)){
+  localStorage.setItem("div1", getdiv(localStorage.getItem("num1"), localStorage.getItem("num2")));
   $("#result").html("Division of two numbers is : " + localStorage.getItem("div1"));
+  }
+  else{
+    throw Error('The given argument is not a number');
+  }
 }
 
 
